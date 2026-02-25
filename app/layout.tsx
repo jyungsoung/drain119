4fbe283fe5244b1db62d45af12e333662967357a
jyungsoung <taesu66@kakao.com>
2026-02-25 12:08:54 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,12 @@ import type React from "react"
 import type { Metadata, Viewport } from "next"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
-import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "응급배관119 | 24시간 하수구 막힘 긴급 출동",
   description:
     "하수구 막힘, 변기 막힘, 싱크대 막힘 24시간 긴급 출동! 30분 내 도착, 무료 견적, 1년 A/S 보장. 지금 바로 전화하세요!",
   keywords: "하수구 막힘, 하수구 뚫음, 변기 막힘, 싱크대 막힘, 배관 청소, 긴급 배관, 24시간 배관",
-  generator: 'v0.app',
   metadataBase: new URL('https://drain119.co.kr'),
   openGraph: {
     type: 'website',
@@ -18,20 +16,7 @@ export const metadata: Metadata = {
     title: "응급배관119 | 24시간 하수구 막힘 긴급 출동",
     description: "하수구 막힘, 변기 막힘, 싱크대 막힘 24시간 긴급 출동! 30분 내 도착, 무료 견적, 1년 A/S 보장",
     siteName: "응급배관119",
-    images: [
-      {
-        url: '/plumber-fixing-pipe-in-dark-background-professiona.jpg',
-        width: 1200,
-        height: 630,
-        alt: '응급배관119 - 하수구 막힘 전문가',
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: "응급배관119 | 24시간 하수구 막힘 긴급 출동",
-    description: "하수구 막힘, 변기 막힘, 싱크대 막힘 24시간 긴급 출동! 30분 내 도착, 무료 견적, 1년 A/S 보장",
-    images: ['/plumber-fixing-pipe-in-dark-background-professiona.jpg'],
+    images: [{ url: '/plumber-fixing-pipe-in-dark-background-professiona.jpg' }],
   },
 }
 
@@ -51,26 +36,21 @@ export default function RootLayout({
       <head>
         <link
           rel="stylesheet"
-          as="style"
-          crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
         />
-        {/* 보라웨어 부정클릭 방지 설정 (ID: j489 반영) */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `var protect_id = 'j489';`,
-          }}
-        />
-        <Script
-          src="//script.boraware.kr/protect_script_v2.js"
-          strategy="afterInteractive"
-        />
+        {/* 보라웨어 부정클릭 방지: 인식률을 높이기 위해 표준 스크립트 방식으로 삽입 */}
+        <script type="text/javascript" dangerouslySetInnerHTML={{
+          __html: `
+            var protect_id = 'j489';
+          `
+        }} />
+        <script type="text/javascript" src="//script.boraware.kr/protect_script_v2.js" async />
       </head>
       <body className="antialiased">
         {children}
         <Analytics />
         
-        {/* 보라웨어 noscript 로그 기록 (ID: j489 반영) */}
+        {/* noscript 로그 기록 */}
         <noscript>
           <img 
             src="//script.boraware.kr/protect_nbora.php?protect_id=j489" 
